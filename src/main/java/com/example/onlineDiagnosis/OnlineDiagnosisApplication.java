bc66bc51bd23a9b5bfa91ba98e33410f860ca02a
PrekWood <nikosprek2000@gmail.com>
2022-05-09 21:24:01 +0300

    Initial commit

---
+++ src/main/java/com/example/onlineDiagnosis/OnlineDiagnosisApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineDiagnosis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineDiagnosisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineDiagnosisApplication.class, args);
+	}
+
+}
