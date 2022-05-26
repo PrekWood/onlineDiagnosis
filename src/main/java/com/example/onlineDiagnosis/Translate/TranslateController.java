88b4640c3cc6874cecbc029bfff348dc77778a68
PrekWood <nikosprek2000@gmail.com>
2022-05-26 20:18:04 +0300

    GET /api/languages

--- src/main/java/com/example/onlineDiagnosis/Translate/TranslateController.java
+++ src/main/java/com/example/onlineDiagnosis/Translate/TranslateController.java
@@ -1,17 +1,34 @@
 package com.example.onlineDiagnosis.Translate;
 
+import ch.qos.logback.core.net.ObjectWriter;
 import com.example.onlineDiagnosis.Model.ApiResponseTranslation;
 import com.example.onlineDiagnosis.SharedClasses.ResponseHandler;
 import com.example.onlineDiagnosis.Translate.TranslateRequest.TranslateRequest;
+import com.example.onlineDiagnosis.User.User;
+import com.example.onlineDiagnosis.User.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
 @RestController
 @AllArgsConstructor
 public class TranslateController extends ResponseHandler {
+
+    private UserService userService;
 
     @CrossOrigin
     @PostMapping("api/translate")
@@ -32,4 +49,42 @@ public class TranslateController extends ResponseHandler {
         }
 
     }
+    @CrossOrigin
+    @GetMapping("api/languages")
+    public ResponseEntity<?> getAcceptedLanguages(){
+
+        // Search for user
+        User loggedInUser = userService.loadUserFromJwt();
+        if (loggedInUser == null) {
+            return createErrorResponse(HttpStatus.FORBIDDEN, "You are not loged in");
+        }
+
+        // Get languages from src/main/resources/languages.json
+        List<HashMap<String, String>> availableLanguages = null;
+        try {
+            File file = ResourceUtils.getFile("classpath:languages.json");
+            Scanner myReader = new Scanner(file);
+
+            // load data as a string
+            StringBuilder availableLanguagesStringBuilder = new StringBuilder();
+            while (myReader.hasNextLine()) {
+                availableLanguagesStringBuilder.append(myReader.nextLine());
+            }
+            myReader.close();
+
+            // json parse
+            availableLanguages = new ObjectMapper().readValue(
+                    availableLanguagesStringBuilder.toString(),
+                    List.class
+            );
+
+        } catch (IOException e) {
+            return createErrorResponse("Error while loading the available languages");
+        }
+
+
+        return createSuccessResponse(availableLanguages);
+
+    }
+
 }
